8ffbef63ac4ca38ad271d3844b5fb06f132243c4
weaming <iweaming@gmail.com>
2016-02-17 22:42:27 +0800

    init

---
+++ app.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask, request, abort, render_template
+from wechatpy import parse_message, create_reply
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import (
+    InvalidSignatureException,
+    InvalidAppIdException,
+)
+from tuling123 import robot
+
+# set token or get from environments
+TOKEN = os.getenv('WECHAT_TOKEN', 'liaojuan520')
+AES_KEY = os.getenv('WECHAT_AES_KEY', '')
+APPID = os.getenv('WECHAT_APPID', '')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    host = request.url_root
+    return render_template('index.html', host=host)
+
+
+@app.route('/wechat', methods=['GET', 'POST'])
+def wechat():
+    signature = request.args.get('signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
+    encrypt_type = request.args.get('encrypt_type', 'raw')
+    msg_signature = request.args.get('msg_signature', '')
+    try:
+        check_signature(TOKEN, signature, timestamp, nonce)
+    except InvalidSignatureException:
+        abort(403)
+    if request.method == 'GET':
+        echo_str = request.args.get('echostr', '')
+        return echo_str
+
+    # POST request
+    if encrypt_type == 'raw':
+        # plaintext mode
+        msg = parse_message(request.data)
+        if msg.type == 'text':
+            if msg.content in ['help', u'帮助']:
+                text = '聊天、笑话、图片、天气、问答、百科、故事、新闻、菜谱、星座、凶吉、成语接龙、快递、飞机、列车、计算'
+            else:
+                text = robot(msg.content, msg.source[:10]) #取消息来源前10位，因为不允许特殊符号
+            reply = create_reply(text, msg)
+        else:
+            reply = create_reply('Sorry, can not handle this for now', msg)
+        return reply.render()
+    else:
+        # encryption mode
+        from wechatpy.crypto import WeChatCrypto
+
+        crypto = WeChatCrypto(TOKEN, AES_KEY, APPID)
+        try:
+            msg = crypto.decrypt_message(
+                request.data,
+                msg_signature,
+                timestamp,
+                nonce
+            )
+        except (InvalidSignatureException, InvalidAppIdException):
+            abort(403)
+        else:
+            msg = parse_message(msg)
+            if msg.type == 'text':
+                reply = create_reply(msg.content, msg)
+            else:
+                reply = create_reply('Sorry, can not handle this for now', msg)
+            return crypto.encrypt_message(reply.render(), nonce, timestamp)
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 9003, debug=True)
---
+++ tuling123.py
@@ -0,0 +1,45 @@
+# coding: utf8
+# 图灵机器人接口
+
+import json
+import requests as rq
+
+token_key = 'e8151bef6a9f9deaf641a7c71b5cb0bc'
+
+def robot(info=u'你好', userid='123'):
+    rt = rq.get('http://www.tuling123.com/openapi/api?key=' + token_key + '&info=' + info + '&userid=' + userid)
+    tl = json.loads(rt.text)
+    # 判断消息类型
+    if tl['code']==100000: #文字
+        text = tl['text']
+    elif tl['code']==200000: #链接
+        text = tl['text']+'\n'+tl['url']
+    elif tl['code']==302000: #新闻
+        text = tl['text']+'\n\n'
+        lenth = len(tl['list'])
+        for index,i in enumerate(tl['list']):
+            text = text + i['article'] + ' [' + i['source'] + ']\n' + i['detailurl']
+            if index < lenth-1:
+                text += '\n\n'
+    elif tl['code']==308000: #菜谱
+        text = tl['text']+'\n\n'
+        max_item = 5
+        for index,i in enumerate(tl['list']):
+            if index < max_item:
+                text = text + i['name'] + ':\n' + i['info'] + '\n' + i['detailurl']
+                if index < max_item-1:
+                    text += '\n\n'
+    elif tl['code']==305000: #列车
+        text = tl['text']+'\n\n'
+        lenth = len(tl['list'])
+        for index,i in enumerate(tl['list']):
+            text = text + i['trainnum'] + ':\n' + i['start'] + '-->' + i['terminal'] + '\n' + i['starttime'] + '--' + i['endtime']
+            if i['detailurl']:
+                text += '\n' + i['detailurl']
+            if index < lenth-1:
+                text += '\n\n'
+    else:
+        text = 'error'
+
+    text = text.replace('<br>','\n')
+    return text
